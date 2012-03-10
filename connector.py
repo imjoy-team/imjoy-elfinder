c9196e8deaaf76feaaf0994ce491f6c5e843fef8
Aleksandr Rakov <aleksandr.rakov@gmail.com>
2012-03-10 12:19:22 +0400

    Some cleanup

--- connector.py
+++ connector.py
@@ -11,7 +11,7 @@ _opts = {
 	'URL': 'http://localhost:8080/static/uploaded',
 	## other options
 	'debug': True,
-	'fileURL': True,  # download files using connector, no direct urls to files
+	'fileURL': True,  # if False: download files using connector, no direct urls to files
 	# 'dirSize': True,
 	# 'dotFiles': True,
 	'fileMode': 0666,
@@ -23,30 +23,25 @@ _opts = {
 
 def connector(request):
     # init connector and pass options
-    elf = elFinder.connector(request.registry.elfinder_options)
-    
+    elf = elFinder.connector(_opts)
+
     # fetch only needed GET/POST parameters
     httpRequest = {}
-    #form = cgi.FieldStorage()
     form=request.params
     for field in elf.httpAllowedParameters:
         if field in form:
-            # Russiam hack
+            # Russian file names hack
             if field == 'name':
                 httpRequest[field] = form.getone(field).encode('utf-8')
 
             elif field == 'targets[]':
-                #httpRequest[field] = form.getlist(field)
                 httpRequest[field] = form.getall(field)
 
             # handle CGI upload
             elif field == 'upload[]':
                 upFiles = {}
                 cgiUploadFiles = form.getall(field)
-                #if not isinstance(cgiUploadFiles, list):
-                #    cgiUploadFiles = [cgiUploadFiles]
                 for up in cgiUploadFiles:
-                    #if hasattr(up, 'file') and hasattr(up, 'filename')
                     if isinstance(up, FieldStorage):
                         upFiles[up.filename.encode('utf-8')] = up.file # pack dict(filename: filedescriptor)
                 httpRequest[field] = upFiles
@@ -58,40 +53,25 @@ def connector(request):
 
     # get connector output and print it out
 
-    # code below is tested with apache only (maybe other server need other method?)
-
-    #if len(header) >= 1:
-    #    for h, v in header.iteritems():
-    #        print h + ': ' + v
-    #    print
     result=Response(status=status)
     try:
         del header['Connection']
     except:
         pass
     result.headers=header
-    #print result.content_type
 
     if not response is None and status == 200:
         # send file
         if 'file' in response and isinstance(response['file'], file):
-            #print "debuuuuuuggggg file"
-            #print response['file'].read()
             result.body=response['file'].read()
             response['file'].close()
 
         # output json
         else:
-            #print json.dumps(response, indent = True)
             result.body=json.dumps(response)
     return result
 
 def includeme(config):
-
-    _opts['root']=config.registry.settings['elfinder_root']
-    _opts['URL']=config.registry.settings['elfinder_url']
-
-    config.registry.elfinder_options=_opts
 
     config.add_route('connector', '/connector')
     config.add_view(connector,route_name='connector')
