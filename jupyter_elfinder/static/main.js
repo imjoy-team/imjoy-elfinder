e5f85d5dcb890a1247bdb6eaa1b527b3cd6cecb6
Wei Ouyang <oeway007@gmail.com>
2020-03-15 01:35:57 +0100

    add app icon (#26)

--- jupyter_elfinder/static/main.js
+++ jupyter_elfinder/static/main.js
@@ -172,8 +172,6 @@
 					'material'      : 'https://nao-pon.github.io/elfinder-theme-manifests/material-default.json',
 					'material-gray' : 'https://nao-pon.github.io/elfinder-theme-manifests/material-gray.json',
 					'material-light': 'https://nao-pon.github.io/elfinder-theme-manifests/material-light.json',
-					'bootstrap'     : 'https://nao-pon.github.io/elfinder-theme-manifests/bootstrap.json',
-					'moono'         : 'https://nao-pon.github.io/elfinder-theme-manifests/moono.json',
 					'win10'         : 'https://nao-pon.github.io/elfinder-theme-manifests/win10.json'
 				},
 				// transport : new elFinderSupportVer1(),
