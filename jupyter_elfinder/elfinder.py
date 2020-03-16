8304799a52a874302f26a352d5a6c10a8da85096
Wei Ouyang <oeway007@gmail.com>
2020-03-16 14:44:27 +0100

    fix typing

--- jupyter_elfinder/elfinder.py
+++ jupyter_elfinder/elfinder.py
@@ -1892,8 +1892,11 @@ def _crop_tuple(size: Tuple[int, int]) -> Optional[Tuple[int, int, int, int]]:
     return None
 
 
-def multi_urljoin(*parts) -> str:
+def multi_urljoin(*parts: str) -> str:
     """ joint multple url parts into a valid url"""
-    return urljoin(
-        parts[0], "/".join(quote_plus(part.strip("/"), safe="/") for part in parts[1:])
+    return str(
+        urljoin(
+            parts[0],
+            "/".join(quote_plus(part.strip("/"), safe="/") for part in parts[1:]),
+        )
     )
