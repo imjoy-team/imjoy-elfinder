24f13a17f63ebb35cb753ac22f9777de2b708767
Wei Ouyang <oeway007@gmail.com>
2020-03-16 18:32:45 +0100

    Fix typo

--- jupyter_elfinder/elfinder.py
+++ jupyter_elfinder/elfinder.py
@@ -340,7 +340,7 @@ class Connector:
                 if thumbs_dir:
                     thumbs_url = self.__path2url(thumbs_dir)
                 else:
-                    thumbs_dir = None
+                    thumbs_url = None
 
                 self._response["options"] = {
                     "path": self._response["cwd"]["rel"],
