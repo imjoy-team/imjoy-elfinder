0c9cbea6669eca3b92e8fe6cdeca122e8ea05dfe
Martin Hjelmare <marhje52@gmail.com>
2020-03-27 13:37:51 +0100

    Include leading dot in mimetype

--- jupyter_elfinder/elfinder.py
+++ jupyter_elfinder/elfinder.py
@@ -132,31 +132,31 @@ COMMANDS = {
 
 MIME_TYPES = {
     # text
-    "cfg": "text/plain",
-    "conf": "text/plain",
-    "css": "text/css",
-    "htm": "text/html",
-    "html": "text/html",
-    "ini": "text/plain",
-    "java": "text/x-java-source",
-    "js": "text/javascript",
-    "md": "text/markdown",
-    "php": "text/x-php",
-    "pl": "text/x-perl",
-    "py": "text/x-python",
-    "rb": "text/x-ruby",
-    "rtf": "text/rtf",
-    "rtfd": "text/rtfd",
-    "sh": "text/x-shellscript",
-    "sql": "text/x-sql",
-    "txt": "text/plain",
+    ".cfg": "text/plain",
+    ".conf": "text/plain",
+    ".css": "text/css",
+    ".htm": "text/html",
+    ".html": "text/html",
+    ".ini": "text/plain",
+    ".java": "text/x-java-source",
+    ".js": "text/javascript",
+    ".md": "text/markdown",
+    ".php": "text/x-php",
+    ".pl": "text/x-perl",
+    ".py": "text/x-python",
+    ".rb": "text/x-ruby",
+    ".rtf": "text/rtf",
+    ".rtfd": "text/rtfd",
+    ".sh": "text/x-shellscript",
+    ".sql": "text/x-sql",
+    ".txt": "text/plain",
     # apps
-    "7z": "application/x-7z-compressed",
-    "doc": "application/msword",
-    "ogg": "application/ogg",
+    ".7z": "application/x-7z-compressed",
+    ".doc": "application/msword",
+    ".ogg": "application/ogg",
     # video
-    "mkv": "video/x-matroska",
-    "ogm": "application/ogm",
+    ".mkv": "video/x-matroska",
+    ".ogm": "application/ogm",
 }
 
 Archivers = TypedDict(  # pylint: disable=invalid-name
@@ -2144,17 +2144,17 @@ def _mimetype(path: str) -> str:
     if mime == "unknown" and ext in mimetypes.types_map:
         mime = mimetypes.types_map[ext]
 
-    if mime == "text/plain" and ext.lstrip(".") == "pl":
-        mime = MIME_TYPES[ext.lstrip(".")]
+    if mime == "text/plain" and ext == ".pl":
+        mime = MIME_TYPES[ext]
 
-    if mime == "application/vnd.ms-office" and ext.lstrip(".") == "doc":
-        mime = MIME_TYPES[ext.lstrip(".")]
+    if mime == "application/vnd.ms-office" and ext == ".doc":
+        mime = MIME_TYPES[ext]
 
     if mime == "unknown":
         if os.path.basename(path) in ["README", "ChangeLog", "LICENSE", "Makefile"]:
             mime = "text/plain"
         else:
-            mime = MIME_TYPES.get(ext.lstrip("."), mime)
+            mime = MIME_TYPES.get(ext, mime)
 
     return mime
 
