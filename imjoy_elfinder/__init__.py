a305769d9fdbab4f157dbe2dee815bcc80490087
Martin Hjelmare <marhje52@gmail.com>
2022-09-09 11:19:57 +0200

    Delint

--- imjoy_elfinder/__init__.py
+++ imjoy_elfinder/__init__.py
@@ -3,4 +3,4 @@ from pathlib import Path
 import json
 
 ROOT_DIR = Path(__file__).parent.resolve()
-__version__ = json.loads((ROOT_DIR / "VERSION").read_text())["version"]
+__version__ = json.loads((ROOT_DIR / "VERSION").read_text(encoding="utf-8"))["version"]
